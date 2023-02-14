b936444b82a1d9e35cfb0a8a1e5343ee53119e19
Emir Bosques <emirbosques@gmail.com>
2023-02-14 01:23:45 +0100

    Destructuring

--- src/app/todos/todo-list/todo-list.component.ts
+++ src/app/todos/todo-list/todo-list.component.ts
@@ -1,8 +1,9 @@
+import { Todo } from './../models/todo.model';
 import { filtrosValidos } from './../../filter/filter.actions';
 import { AppState } from './../../app.reducer';
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Todo } from '../models/todo.model';
+
 
 @Component({
   selector: 'app-todo-list',
@@ -18,10 +19,11 @@ export class TodoListComponent implements OnInit {
 
   }
 
+
   ngOnInit(): void {
-    this.store.subscribe(state => {
-      this.todoList = state.todos;
-      this.filterSelected = state.filter;
+    this.store.subscribe( ({todos, filter }) => {
+      this.todoList = todos;
+      this.filterSelected = filter;
     });
   }
 
